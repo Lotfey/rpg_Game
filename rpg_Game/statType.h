38177227990b1ea988cfd7a143561169944d2ef9
Lotfi <achirilotfi2@gmail.com>
2022-12-20 12:06:46 +0100

    typos

--- rpg_Game/statType.h
+++ rpg_Game/statType.h
@@ -5,5 +5,4 @@ typedef std::uint16_t statType;
 // This type was intially uint8_t which intruduces few buggs while 
 // printing the values with std::cout.
 // this could be due to the formating issue
-// therefore, for a quick fix, i used uint16_t
-
+// therefore, for a quick fix, i used uint16_
